677201437b46b732aef856a59cfd4a7b257b031a
gorka <gorka@DESKTOP-7EKUIUG>
2025-04-26 18:55:06 +0200

    Tercer cambio 2

--- proPruebas/src/paqPruebas/HolaMundo.java
+++ proPruebas/src/paqPruebas/HolaMundo.java
@@ -3,6 +3,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Adios Mundo");
-        System.out.println("Tercer cambio");
+        System.out.println("Tercer cambio 2");
     }
 }
