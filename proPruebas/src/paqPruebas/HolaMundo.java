2327c01743cb7435e42ab97e765e10771557f943
DhazZzz <110050353+DhazZzz@users.noreply.github.com>
2025-04-26 18:30:08 +0200

    Segunda Version

--- proPruebas/src/paqPruebas/HolaMundo.java
+++ proPruebas/src/paqPruebas/HolaMundo.java
@@ -2,6 +2,7 @@ package paqPruebas;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Adios Mundo");
     }
     
 }
